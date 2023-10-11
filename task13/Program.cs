edcc48420e20169ea616eb137730c8979533b257
killtonik <skoromnyj98@gmail.com>
2023-10-11 20:52:12 +0300

    Дз 2 семинара

---
+++ task13/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/ 
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine(number);
+int[] array = {number};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1
+    index++;
+}
+
+
+
+
+
+
+
+
+// {
+//      System.Console.WriteLine($"Третья цыфра {number}");
+// } 
+// else
+// System.Console.WriteLine("третьей цифры нет");
