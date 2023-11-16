1a5829abe8637b502ec74ef0fbce6a0f1e2f8a11
killtonik <skoromnyj98@gmail.com>
2023-11-16 14:24:22 +0300

    рекурсия

---
+++ task67_рекурсия/Program.cs
@@ -0,0 +1,16 @@
+﻿// 67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+
+int Summ(int num)
+{
+    if(num == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return num % 10 + Summ(num/10);
+    }
+}
+int result = Summ(123);
+System.Console.WriteLine(result);
